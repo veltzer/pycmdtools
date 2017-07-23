073e50f448d595a6f61998c9cc60cca2eba89fe5
Mark Veltzer <mark.veltzer@gmail.com>
2017-07-23 16:07:14 +0300

    bugfix

--- src/pycmdtools/scripts/change_first_line.py
+++ src/pycmdtools/scripts/change_first_line.py
@@ -33,22 +33,33 @@ def main(
 ) -> None:
     """
         Validate json files
+        :param from_line: what text to change
+        :param to_line: what to change it to
         :param filenames: filenames to change the first line of
         :return: nothing
     """
     setup()
+    changed = 0
+    actually_changed = 0
     print("from_line is [{}]".format(from_line))
     print("to_line is [{}]".format(to_line))
     for filename in filenames:
+        print("considering [{}]...".format(filename))
         with open(filename, "rt") as input_handle:
             data = input_handle.readlines()
         if len(data) == 0:
             continue
         # change the first line
         if from_line is None or data[0] == from_line+"\n":
+            if data[0] != to_line+"\n":
+                actually_changed += 1
             data[0] = to_line+"\n"
+            changed += 1
         with open(filename, "wt") as output_handle:
             output_handle.write("".join(data))
+    # print statistics
+    print("changed is [{}]".format(changed))
+    print("actually_changed is [{}]".format(actually_changed))
 
 
 if __name__ == '__main__':
