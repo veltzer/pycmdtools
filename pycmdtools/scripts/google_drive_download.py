ccde1d97488b37e28af8b5132c078fea6200a806
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-18 05:45:34 +0300

    better params

--- pycmdtools/scripts/google_drive_download.py
+++ pycmdtools/scripts/google_drive_download.py
@@ -2,23 +2,8 @@ import click
 import requests
 from pylogconf import setup
 
-"""
-This script downloads stuff from google drive.
-If you have a link to a google drive file like this:
-    https://drive.google.com/open?id=0BwNoUKizWBdnRmRmLVlWSWxzWnM
-or like this:
-    https://drive.google.com/file/d/0BwNoUKizWBdnSHF4SFlyRkxNSVE/view?usp=sharing
-Then the file id of the relevant files are:
-    0BwNoUKizWBdnRmRmLVlWSWxzWnM
-    0BwNoUKizWBdnSHF4SFlyRkxNSVE
-And this is what you have to supply to this script to download the files.
 
-References:
-- http://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive
-"""
-
-
-def download_file_from_google_drive(file_id, destination):
+def download_file_from_google_drive(file_id: str, destination: str):
     url = "https://docs.google.com/uc?export=download"
 
     session = requests.Session()
@@ -37,7 +22,6 @@ def get_confirm_token(response):
     for key, value in response.cookies.items():
         if key.startswith('download_warning'):
             return value
-
     return None
 
 
@@ -53,10 +37,16 @@ def save_response_content(response, destination):
 @click.argument(
     'file_id',
     required=True,
+    type=str,
+    help='id of the google drive document',
+    show_default=True,
 )
 @click.argument(
     'destination',
     required=True,
+    type=str,
+    help='file name to save',
+    show_default=True,
 )
 def main(
         file_id: str,
@@ -64,6 +54,18 @@ def main(
 ) -> None:
     """
     Download a file from a google drive using it's id.
+
+    If you have a link to a google drive file like this:
+    https://drive.google.com/open?id=0BwNoUKizWBdnRmRmLVlWSWxzWnM
+    or like this:
+    https://drive.google.com/file/d/0BwNoUKizWBdnSHF4SFlyRkxNSVE/view?usp=sharing
+    Then the file id of the relevant files are:
+    0BwNoUKizWBdnRmRmLVlWSWxzWnM
+    0BwNoUKizWBdnSHF4SFlyRkxNSVE
+    And this is what you have to supply to this script to download the files.
+
+    References:
+    - http://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive
     :param file_id: 
     :param destination: 
     :return: 
