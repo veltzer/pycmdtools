e332d0446be81e1825b85b3f99478890cd367422
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-30 21:51:33 +0300

    new version after pycharm

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pycmdtools',
-    version='0.0.57',
+    version='0.0.58',
     packages=[
         'pycmdtools',
         'pycmdtools.scripts',
