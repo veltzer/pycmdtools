28d4692a5d24ea74328f46a8afb000ae9f562631
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-07 17:51:25 +0300

    bring license back

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     ],
     url="https://veltzer.github.io/pycmdtools",
     download_url="https://github.com/veltzer/pycmdtools",
-    # license="MIT",
+    license="MIT",
     platforms=[
         'python3',
     ],
