d172b9735c64ecce9929d6665e035c08d38e7775
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-28 11:35:47 +0300

    new version

--- pycmdtools/static.py
+++ pycmdtools/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.83"
+VERSION_STR = "0.0.84"
 DESCRIPTION = "pycmdtools is set of useful command line tools written in python"
 APP_NAME = "pycmdtools"
 LOGGER_NAME = "pycmdtools"
