75b16edddf7953fe4a5cb1e8094c4379321eed1d
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 01:25:03 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -29,8 +29,8 @@ build_requires: list[str] = [
 test_requires: list[str] = [
     "pylint",
     "pytest",
-    "pytest-cov",
     "mypy",
+    "ruff",
     # types
     "types-PyYAML",
     "pandas-stubs",
