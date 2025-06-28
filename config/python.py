daa1f206d5adb84a1963ccda5396bc765c79e588
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 21:46:13 +0300

    more

--- config/python.py
+++ config/python.py
@@ -1,8 +1,8 @@
 """ python deps for this project """
 
-console_scripts: list[str] = [
-    "pycmdtools=pycmdtools.main:main",
-]
+scripts: dict[str,str] = {
+    "pycmdtools": "pycmdtools.main:main",
+}
 config_requires: list[str] = [
     "pyclassifiers",
 ]
---
+++ pycmdtools/static.py
@@ -0,0 +1,5 @@
+""" version which can be consumed from within the module """
+VERSION_STR = "0.0.85"
+DESCRIPTION = "pycmdtools is set of useful command line tools written in python"
+APP_NAME = "pycmdtools"
+LOGGER_NAME = "pycmdtools"
