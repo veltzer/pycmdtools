b2ebd91e6a18c83c0e83d6c253f04132bd3a5aaa
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:33:33 +0300

    fixed up deps

--- pycmdtools/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.85"
-DESCRIPTION = "pycmdtools is set of useful command line tools written in python"
-APP_NAME = "pycmdtools"
-LOGGER_NAME = "pycmdtools"
