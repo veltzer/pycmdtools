ff24056eb7fd21c1e9d2d3c2f070ff25cfa9b639
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-23 23:51:08 +0300

    move to rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,9 @@
+NAME = "pycmdtools"
+DESCRIPTION_SHORT = "pycmdtools is set of useful command line tools written in python"
+KEYWORDS = {
+    "utils",
+    "command-line",
+    "python",
+    "shell",
+    "utilities",
+}
